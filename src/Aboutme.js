79cba11dd081f729660bf2376c8246955adb9916
Pablo Garcia <pgarciag566@gmail.com>
2022-08-05 18:16:15 -0500

    aded react components for each of my page sections

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 export default function Aboutme() {
   return (
-    <div>
-        <div class ="justify-center">
-            <h1 class = "text-3xl mx-8 md:mx-32 xl:mx-64 2xl:ml-96">Fullstack Web Developer and Finance Student</h1>
+    <div class = "mt-16">
+        <div class ="justify-center my-5 md:w-5/6">
+            <h1 class = "text-3xl font-medium mx-8 md:mx-32 md:text-6xl md:font-light xl:mx-64 2xl:ml-96">Full Stack Developer and Finance Student</h1>
         </div>
             
-        <div class ="justify-center md:flex">
-            <p class ="text-lg mx-8 md:ml-32  xl:ml-64 2xl:ml-96">Fullstack Web developer with a passion for learning new and exciting things! Positive thinker interested in technology and finance trends. Intermediate skills in HTML and CSS. Begginner skills in Javascript front-end and back-end </p>
-            <img id="starterImg" class = "m-auto  h-96 md:mr-32 xl:mx-64 2xl:mr-96 rounded-lg" src={process.env.PUBLIC_URL+"favicons/starterPic.jpg"}></img>
+        <div class ="justify-center my-5 md:flex md:justify-between my-5">
+            <div class ="text-3xl my-5 font-light mx-8 md:ml-32 md:w-1/3  xl:ml-64 2xl:ml-96">
+            <p>Full Stack developer with a passion for learning new and exciting things! Positive thinker interested in technology and financial trends.</p>
+            <p class="mt-8">Advanced skills in HTML and CSS. Intermediate skills in Javascript front-end and back-end. Excelent in database management.</p>
+            </div>
+            <img id="starterImg" class = " justify-center h-96 md:mr-32 xl:mr-64 2xl:mr-96 rounded-lg" src={process.env.PUBLIC_URL+"favicons/starterPic.jpg"}></img>
+        </div>
+
+        <div class ="justify-center text-center mt-32 ">
+            <h1 class = "text-3xl overline underline font-light mx-8 md:mx-32 xl:mx-64 2xl:ml-96">My Projects</h1>
         </div>
 
     </div>
--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './Header';
 import Modal from './Modal';
 import Aboutme from './Aboutme';
+import Projects from './Projects';
 
 function App() {
   return (
     <>
       <Header />
+      <Modal />
       <Aboutme />
+      <Projects />
       </>
   );
 }
