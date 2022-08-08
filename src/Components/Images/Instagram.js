3b428d778ce6c7ffec7392d75fb9e9d8847e8611
Pablo Garcia <pgarciag566@gmail.com>
2022-08-07 23:09:51 -0500

    temporarily added an href for the instagram logo

--- src/Components/Images/Instagram.js
+++ src/Components/Images/Instagram.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Instagramimage from "./instagram.png"
+
 
 export default function Instagram() {
   return (
-    <img src={Instagramimage} width="20px" height="20px"></img>
+    <img src="https://cdn-icons.flaticon.com/png/512/717/premium/717392.png?token=exp=1659929937~hmac=e87e5fc707545665cd0ced994840db9d" width="20px" height="20px"></img>
   )
 }
 
