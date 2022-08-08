87c2a082ada343c012b2db1d632ef730ea36fec5
Pablo Garcia <pgarciag566@gmail.com>
2022-08-07 22:14:11 -0500

    removed unesesary imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import Header from './Components/Header';
-import Modal from './Components/Modal';
 import Aboutme from './Components/Aboutme';
 import Projects from './Components/Projects';
+import Button from './Components/Button';
+import Modal from './Components/Modal';
+
 
 function App() {
+
   return (
     <>
       <Header />
-      <Modal />
+      <Button />
+      
       <Aboutme />
       <Projects />
-      </>
+    </>
   );
 }
 
 export default App;
+
+
