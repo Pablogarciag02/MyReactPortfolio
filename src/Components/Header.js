04019f05473e26dae27d1ae1f75026a13a439922
Pablo Garcia <pgarciag566@gmail.com>
2022-08-07 22:14:29 -0500

    removed button that i turned into a component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Header.css"
+import Linkedin from './Images/Linkedin'
+import Instagram from './Images/Instagram'
+import Github from './Images/Github'
 
 export default function Header() {
   return (
@@ -8,30 +11,21 @@ export default function Header() {
         <span className = "mr-8 hover:underline"><a href="mailto:pgarciag566@gmail.com">Contáctame</a></span>
         <span className = "mr-8 hover:underline"><a href="https://drive.google.com/file/d/1CXXbnb9I8H2h2DM6dfzdsSiLOUfg7lL0/view?usp=sharing" target="_blank">Resumé</a></span>
 
-        <li className ="inline list-none mx-4"><a href="https://github.com/Pablogarciag02" target="_blank"><img src={process.env.PUBLIC_URL+"favicons/github.png"}
-          width="20px"
-          height="20px"></img></a>
+        <li className ="inline list-none mx-4"><a href="https://github.com/Pablogarciag02" target="_blank">
+          <Github />
+        </a>
         </li>
-        <li className ="inline list-none mx-4 "><a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-08842621b/" target="_blank"><img src={process.env.PUBLIC_URL+"favicons/linkedin.png"}
-          width="20px"
-          height="20px"></img></a>
+
+        <li className ="inline list-none mx-4 "><a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-08842621b/" target="_blank">
+          <Linkedin />
+        </a>
         </li>
-        <li className ="inline list-none mx-4"> <a href="https://www.instagram.com/pablogarciag02/" target="_blank"> <img src={process.env.PUBLIC_URL+"favicons/instagram.png"}
-          width="20px"
-          height="20px"></img></a>
+
+        <li className ="inline list-none mx-4"> <a href="https://www.instagram.com/pablogarciag02/" target="_blank"> 
+          <Instagram />
+        </a>
         </li>
       </div>
-
-
-      <div className ="float-right mr-8 lg:invisible ">
-          <button id = "square" className ="" href="">
-              <div id = "burgerwrap" className="">
-                <span></span>
-            
-                <span></span>
-              </div>
-          </button>
-      </div> 
     </div>
   )
 }
