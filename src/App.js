b0ab08dce59d1c05f96ba3ed47b52b49997fefd2
Pablo Garcia <pgarciag566@gmail.com>
2022-08-08 00:31:05 -0500

    added ims to components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './Components/Header';
 import Aboutme from './Components/Aboutme';
 import Projects from './Components/Projects';
 import Button from './Components/Button';
-import Modal from './Components/Modal';
+
 
 
 function App() {
