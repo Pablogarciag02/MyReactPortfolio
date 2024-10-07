68605f0d7e4792232807df6baafe37372b5e62e6
Pablo Garcia <pgarciag566@gmail.com>
2024-10-07 11:37:45 -0600

    changed resume

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,21 +7,21 @@ import Github from './Images/Github'
 export default function Header() {
   return (
     <div>
-      <div className="flex justify-end mt-14 mr-24  invisible lg:visible">
+      <div className="flex justify-end invisible mr-24 mt-14 lg:visible">
         <span className = "mr-8 hover:underline"><a href="mailto:pgarciag566@gmail.com">Contáctame</a></span>
-        <span className = "mr-8 hover:underline"><a href="https://drive.google.com/file/d/1CXXbnb9I8H2h2DM6dfzdsSiLOUfg7lL0/view?usp=sharing" target="_blank">Resumé</a></span>
+        <span className = "mr-8 hover:underline"><a href="https://drive.google.com/file/d/13IXTFX5UfO4RmfzzAnW2ysQoRMDSX937/view?usp=sharing" target="_blank">Resumé</a></span>
 
-        <li className ="inline list-none mx-4"><a href="https://github.com/Pablogarciag02" target="_blank">
+        <li className ="inline mx-4 list-none"><a href="https://github.com/Pablogarciag02" target="_blank">
           <Github />
         </a>
         </li>
 
-        <li className ="inline list-none mx-4 "><a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-08842621b/" target="_blank">
+        <li className ="inline mx-4 list-none "><a href="https://www.linkedin.com/in/pablo-garc%C3%ADa-08842621b/" target="_blank">
           <Linkedin />
         </a>
         </li>
 
-        <li className ="inline list-none mx-4"> <a href="https://www.instagram.com/pablogarciag02/" target="_blank"> 
+        <li className ="inline mx-4 list-none"> <a href="https://www.instagram.com/pablogarciag02/" target="_blank"> 
           <Instagram />
         </a>
         </li>
